6670149f5eeb206ec4182178637cd61c435feead
Michael <mgolden6@gmail.com>
2023-06-12 23:28:42 -0400

    fix_theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,39 @@
 import React, { useMemo } from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import { Box, CssBaseline, ThemeProvider } from "@mui/material"
 import { createTheme, responsiveFontSizes } from "@mui/material/styles"
 
 import themeSettings from "@/theme/themeSettings"
-import Navbar from "@/components/Navbar"
+import Navbar from "@/features/navbar/Navbar"
+import Home from "@/features/home/Home"
+import Devices from "@/features/devices/Devices"
+import Groups from "@/features/groups/Groups"
+import Measurements from "@/features/measurements/Measurements"
+import Users from "@/features/users/Users"
+import { useAppSelector } from "@/hooks/useStore"
 
 function App() {
-  let theme = useMemo(() => createTheme(themeSettings), [])
+  const mode = useAppSelector((state) => state.navbar.mode)
+  let theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   theme = responsiveFontSizes(theme)
 
   return (
-    <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Box height="100%" width="100%" padding="1rem 2rem 4rem 2rem">
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<div>Home</div>} />
-              <Route path="/Devices" element={<div>Devices</div>} />
-            </Routes>
-          </Box>
-        </ThemeProvider>
+        <Box height="100%" width="100%" padding="1rem 2rem 4rem 2rem">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/devices" element={<Devices />} />
+            <Route path="/groups" element={<Groups />} />
+            <Route path="/measurements" element={<Measurements />} />
+            <Route path="/users" element={<Users />} />
+          </Routes>
+        </Box>
       </BrowserRouter>
-    </React.StrictMode>
+    </ThemeProvider>
   )
 }
 
--- src/theme/themeSettings.ts
+++ src/theme/themeSettings.ts
@@ -1,30 +1,14 @@
-const themeSettings = {
-  palette: {
-    primary: {
-      main: "rgb(163, 201, 255)",
-      dark: "rgb(163, 201, 255)",
-      light: "rgb(0, 96, 170)",
+import { PaletteMode } from "@mui/material"
+
+const themeSettings = (mode: PaletteMode) => {
+  return {
+    palette: {
+      mode,
     },
-    secondary: {
-      main: "rgb(188, 199, 219)",
-      dark: "rgb(188, 199, 219)",
-      light: "rgb(84, 95, 112)",
+    typography: {
+      fontFamily: "Comfortaa",
     },
-    error: {
-      main: "rgb(255, 180, 171)",
-      dark: "rgb(255, 180, 171)",
-      light: "rgb(186, 26, 26)",
-    },
-    tertiary: {
-      main: "rgb(217, 189, 227)",
-      dark: "rgb(217, 189, 227)",
-      light: "rgb(109, 86, 119)",
-    },
-    background: {
-      default: "rgb(26, 28, 30)",
-      light: "rgb(253, 252, 255)",
-    },
-  },
+  }
 }
 
 export default themeSettings
