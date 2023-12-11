b36827dbe64ae9509b7c475cfbfbfa6af07849b0
Michael <mgolden6@gmail.com>
2023-12-11 16:09:37 -0500

    try_heroku

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_BASE_URL,
+    //? change back for Vite if Heroku doesn't work
+    // baseUrl: import.meta.env.VITE_API_BASE_URL,
+    baseUrl: process.env.API_BASE_URL,
   }),
   tagTypes: ["Devices", "Mobiles", "Groups", "Measurements", "AveragesAndStds"],
   endpoints: (builder) => ({
